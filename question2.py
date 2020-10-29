ebba5de5cdd0ee5aca543830ce5ba83c2b15f321
Nathan Brown <scottynate05@yahoo.com>
2020-10-28 19:44:03 -0700

    question 2 done

---
+++ question2.py
@@ -0,0 +1,25 @@
+# Create a function that returns True if the given string has any of the following:
+
+# Only letters and no numbers.
+# Only numbers and no letters.
+# If a string has both numbers and letters or contains characters that don't fit into any category, return False.
+
+# Examples:
+
+# csAlphanumericRestriction("Bold") ➞ True
+# csAlphanumericRestriction("123454321") ➞ True
+# csAlphanumericRestriction("H3LL0") ➞ False
+# Notes:
+
+# Any string that contains spaces or is empty should return False.
+# [execution time limit] 4 seconds (py3)
+
+# [input] string input_str
+
+# [output] boolean
+
+def csAlphanumericRestriction(input_str):
+    if input_str.isalpha() or input_str.isdigit():
+        return True
+    else:
+        return False
